0f50df7d38461e41464c31266ffaed6444ca4062
string <pcollie4@vols.utk.edu>
2024-03-21 08:44:30 -0400

    data

---
+++ run.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from multiprocessing import Pool, cpu_count
+
+inertia_default = 0.5
+particle_default = 40
+cognition_default = 1
+social_default = 1
+
+for particle in range(10,101,10):
+    break
+    for i in range(20):
+        command = f"python3 pso.py --num_particles {particle} --inertia {inertia_default} --cognition {cognition_default} --social {social_default} --gen {i}"
+        os.system(command)
+
+for inertia in range(1, 11):
+    break
+    dec_inertia = inertia/10
+    if dec_inertia == inertia_default:
+        continue
+    for i in range(20):
+        command = f"python3 pso.py --num_particles {particle_default} --inertia {dec_inertia} --cognition {cognition_default} --social {social_default} --gen {i}"
+        os.system(command)
+
+def run_set(cognition):
+    dec_cog = cognition/10
+    for social in range(1, 41):
+        dec_social = social/10
+        if dec_social == social_default and dec_cog == cognition_default:
+            continue
+        for i in range(20):
+            command = f"python3 pso.py --num_particles {particle_default} --inertia {inertia_default} --cognition {dec_cog} --social {dec_social} --gen {i}"
+            os.system(command)
+
+    return cognition
+
+if __name__ == "__main__":
+    cognitions = [i for i in range(8,41)]
+    with Pool(cpu_count()) as p:
+        print(p.map(run_set, cognitions))
